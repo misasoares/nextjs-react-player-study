496e1439572b92dbf8355f3480527c60d651da8c
misasoares <misasoaresms2@gmail.com>
2024-03-05 19:00:12 -0300

    teste de novas propriedades

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import { Typography } from "@mui/material";
+"use client";
+import { CheckCircle, CheckOutlined } from "@mui/icons-material";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
+import { Button, LinearProgress, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import { Inter } from "next/font/google";
+import Slider from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
 import { useState } from "react";
 import ReactPlayer from "react-player";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
@@ -14,9 +17,18 @@ export default function Home() {
     "https://www.youtube.com/watch?v=wmoCIHlA2bA",
   ];
 
+  const [progress, setProgress] = useState<number>(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const [volume, setVolume] = useState<number>(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setVolume(newValue as number);
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-5 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-5`}
     >
       <Grid container spacing={2}>
         <Grid item xs={12}>
@@ -28,8 +40,39 @@ export default function Home() {
 
         <Grid item xs={12} className="w-full flex justify-center">
           <ReactPlayer
-            playing
             url="https://www.youtube.com/watch?v=ggB0fVqVobs"
+            volume={volume / 100} // 0 até 1
+            config={{
+              youtube: {
+                playerVars: { showinfo: 1 },
+              },
+            }}
+            controls
+            onProgress={({ loaded }) => setProgress(loaded)}
+            onPlay={() => setIsPlaying(true)}
+            onPause={() => setIsPlaying(false)}
+          />
+          <Button variant="contained"></Button>
+        </Grid>
+        <Grid item xs={12}>
+          {isPlaying ? <CheckCircle color="success" /> : <CheckOutlined />}
+        </Grid>
+        <Grid item xs={12}>
+          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+            <VolumeDown />
+            <Slider
+              aria-label="Volume"
+              value={volume}
+              onChange={handleChange}
+            />
+            <VolumeUp />
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <LinearProgress
+            color="error"
+            variant="determinate"
+            value={progress * 100}
           />
         </Grid>
 
@@ -43,8 +86,8 @@ export default function Home() {
               width={360}
               url={url}
               playing={activeIndex === index}
-              onMouseEnter={() => setActiveIndex(index)}
-              onMouseLeave={() => setActiveIndex(null)}
+              // onMouseEnter={() => setActiveIndex(index)}
+              // onMouseLeave={() => setActiveIndex(null)}
             />
           </Grid>
         ))}
